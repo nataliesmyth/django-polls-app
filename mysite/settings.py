f167c5ca88ec401e3f6f8132a7f8edbfc3750b68
Nataliesmyth <nataliesmyth11@gmail.com>
2021-07-08 15:54:22 -0400

    sql db

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -75,10 +77,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase', 
     }
 }
+
 
 
 # Password validation
